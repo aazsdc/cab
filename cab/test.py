846dfe46400d3734bc05dd34b26ea72df4878a3f
1301-9 <acig116361@naver.com>
2023-10-12 15:11:17 +0900

    ma

--- cab/test.py
+++ cab/test.py
@@ -1,32 +1,69 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
+import pandas as pd
+from PyQt5.QtWidgets import (QApplication, QTableWidget, QTableWidgetItem, QVBoxLayout, QPushButton,
+                             QFileDialog, QWidget, QLabel, QProgressBar)
+from PyQt5.QtCore import QTimer
 
-#UI파일 연결
-#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("gui.ui")[0]
 
-#메인 윈도우 클래스
-class WindowClass(QMainWindow, form_class) :
-    #초기화 메서드
-    def __init__(self) :
+class DatasetViewer(QWidget):
+    def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        #pushButton (시작버튼)을 클릭하면 아래 fuctionStart 메서드와 연결 됨.
-        self.pushButton.clicked.connect(self.functionStart)
 
-    # 시작버튼을 눌렀을 때 실행되는 메서드
-    def functionStart(self):
-      self.progressBar.setRange(0, 19) #progressbar 초기 설정(100을 0~19, 20단계로 나눔)
-      for i in range(0, 20):
-            print("출력 : ",str(i))
-            self.progressBar.setValue(i) #progress bar 진행률 올리기
-            self.textBrowser.append("출력 : "+str(i)) #text browser 문자열 추가하기
+        layout = QVBoxLayout()
+        self.tableWidget = QTableWidget()
 
-#코드 실행시 GUI 창을 띄우는 부분
-#__name__ == "__main__" : 모듈로 활용되는게 아니라 해당 .py파일에서 직접 실행되는 경우에만 코드 실행
-if __name__ == "__main__" :
-    app = QApplication(sys.argv)
-    myWindow = WindowClass()
-    myWindow.show()
-    app.exec_()
+        self.loadButton = QPushButton('Load Dataset')
+        self.loadButton.clicked.connect(self.loadDataset)
+
+        self.uniqueLabel = QLabel()
+        self.progressBar = QProgressBar(self)
+        self.progressBar.setMaximum(100)
+
+        layout.addWidget(self.loadButton)
+        layout.addWidget(self.uniqueLabel)
+        layout.addWidget(self.progressBar)
+        layout.addWidget(self.tableWidget)
+
+        self.setLayout(layout)
+        self.show()
+
+    def loadDataset(self):
+        filepath, _ = QFileDialog.getOpenFileName()
+        if filepath:
+            self.progressBar.setValue(0)
+            QTimer.singleShot(100, lambda: self.processData(filepath))
+
+    def processData(self, filepath):
+        dataset = pd.read_csv(filepath)
+        self.progressBar.setValue(30)
+
+        unique_values = dataset['normal.'].unique()
+        self.uniqueLabel.setText(f"Unique values before replacement: {unique_values}")
+
+        # 카테고리 값 변경
+        attack_categories = ['buffer_overflow.', 'loadmodule.', 'perl.', 'neptune.',
+                             'smurf.', 'guess_passwd.', 'pod.', 'teardrop.', 'portsweep.',
+                             'ipsweep.', 'land.', 'ftp_write.', 'back.', 'imap.', 'satan.',
+                             'phf.', 'nmap.', 'multihop.', 'warezmaster.', 'warezclient.',
+                             'spy.', 'rootkit.']
+        dataset['normal.'] = dataset['normal.'].replace(attack_categories, 'attack')
+
+        unique_values_after = dataset['normal.'].unique()
+        self.uniqueLabel.setText(f"Unique values after replacement: {unique_values_after}")
+        self.progressBar.setValue(60)
+
+        self.displayDataset(dataset.head())
+        self.progressBar.setValue(100)
+
+    def displayDataset(self, df):
+        self.tableWidget.setRowCount(df.shape[0])
+        self.tableWidget.setColumnCount(df.shape[1])
+
+        for i in range(df.shape[0]):
+            for j in range(df.shape[1]):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(df.iloc[i, j])))
+
+
+app = QApplication(sys.argv)
+window = DatasetViewer()
+sys.exit(app.exec_())
